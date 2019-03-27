fa02a7a8458aa608086b2195eaf8931054e2a09d
thundernet8 <wuxueqian2010@hotmail.com>
2019-03-27 21:31:19 +0800

    clean

--- src/core/injection/injectable.ts
+++ src/core/injection/injectable.ts
@@ -12,27 +12,17 @@ const DEFAULT_INJECTABLE_OPTIONS: InjectableOptions = {
 export default function Injectable(options?: InjectableOptions | TConstructor): any {
     let mergedOptions: any;
     if (isFunction(options)) {
-        mergedOptions = Object.assign({}, DEFAULT_INJECTABLE_OPTIONS);
-
-        // StoreService should not be cacheable, it can be used in different vue component instances but the state is not sharable
-        if (isStoreService(options)) {
-            mergedOptions.cacheable = false;
-        }
-
         Reflect.defineMetadata(INJECTABLE_FLAG, true, options as TConstructor);
         StaticInjector.registerProvider({
             token: options as TConstructor,
-            cacheable: mergedOptions.cacheable,
+            cacheable: DEFAULT_INJECTABLE_OPTIONS.cacheable,
             value: EMPTY
         });
         return options;
     }
     mergedOptions = Object.assign({}, DEFAULT_INJECTABLE_OPTIONS, options || {});
     return function(target: TConstructor) {
-        // const paramTypes = Reflect.getMetadata('design:paramtypes', target);
         Reflect.defineMetadata(INJECTABLE_FLAG, true, target);
-        // Reflect.defineMetadata(INJECTION_CACHEABLE_FLAG, mergedOptions.cacheable, target);
-        // Reflect.defineMetadata(INJECTED_PARAMS_METADATA_KEY, paramTypes, target);
         StaticInjector.registerProvider({
             token: target,
             cacheable: mergedOptions.cacheable,
