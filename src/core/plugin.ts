2f6e0f460b5596783e7f5dc15f65adb3ddad96ab
thundernet8 <wuxueqian2010@hotmail.com>
2019-04-19 15:54:28 +0800

    opt store binding and vue mixin code

---
+++ src/core/plugin.ts
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import { VueConstructor } from 'vue/types/vue';
+import { Store } from 'vuex';
+import { handleStoreServiceBinding, isStoreService } from './store/vuex';
+import { componentInstanceMap } from './component/decotator';
+import { isFunction } from './utils/index';
+
+let velayStore: Store<any>;
+const vueInstance = new Vue();
+
+const plugin = {
+    install(_Vue: VueConstructor) {
+        _Vue.mixin({
+            beforeCreate() {
+                const options = this.$options;
+
+                // store injection
+                if (!velayStore) {
+                    if (options.store) {
+                        velayStore = typeof options.store === 'function' ? (options as any).store() : options.store;
+                    } else if (options.parent && options.parent.$velayStore) {
+                        velayStore = options.parent.$velayStore;
+                    } else if (options.parent && options.parent.$store) {
+                        velayStore = options.parent.$store;
+                    }
+                }
+
+                this.$velayStore = velayStore;
+
+                // service injection
+                const vid = (this.$options as any)._vid;
+                const instance = componentInstanceMap[vid];
+                if (!instance) {
+                    return;
+                }
+                const vuePropKeys = Object.getOwnPropertyNames(vueInstance);
+                const paramKeys = Object.getOwnPropertyNames(instance).filter(k => !vuePropKeys.includes(k));
+                paramKeys.forEach(key => {
+                    // auto injected services must be functions or customized store service
+                    if (isFunction(instance[key]) || isStoreService(instance[key])) {
+                        handleStoreServiceBinding(instance[key], this);
+                        (this as any)[key] = instance[key];
+                    }
+                });
+
+                // dispose instance
+                componentInstanceMap[vid] = null;
+            }
+        });
+    }
+};
+
+export default plugin;
